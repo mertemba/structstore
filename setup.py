e5e1218aaa56d30a98b700f82443d2952c492408
Max Mertens <max.mail@dameweb.de>
2022-12-08 14:42:47 +0100

    Added YAML dependency in setup.py.

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup
 
 ext_modules = [
     Pybind11Extension(
-        "structstore",
-        ["src/structstore_pybind.cpp"],
-        extra_link_args=["-lrt"],
+        'structstore',
+        ['src/structstore_pybind.cpp'],
+        extra_link_args=['-lrt', '-lyaml-cpp'],
         extra_compile_args=['-std=c++17'],
     ),
 ]
 
-setup(name="structstore",
-      version="0.0.1",
-      description="Structured object storage, dynamically typed, to be shared between processes",
-      author="Max Mertens",
-      author_email="max.mail@dameweb.de",
-      license="BSD-3-Clause",
+setup(name='structstore',
+      version='0.0.1',
+      description='Structured object storage, dynamically typed, to be shared between processes',
+      author='Max Mertens',
+      author_email='max.mail@dameweb.de',
+      license='BSD-3-Clause',
       zip_safe=False,
-      python_requires=">=3.6",
+      python_requires='>=3.6',
       ext_modules=ext_modules,
       include_package_data=True,
       install_requires=[
-          "pybind11",
+          'pybind11',
       ])
