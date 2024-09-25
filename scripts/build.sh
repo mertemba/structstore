dda623391d83a8d1fae49892d2aae92568797343
Max Mertens <max.mail@dameweb.de>
2024-09-25 08:47:18 +0200

    Add coverage flags in CI tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,6 +16,9 @@ fi
 if [ ! -z "$BUILD_WITH_TESTING" ]; then
     cmake_options="$cmake_options -DBUILD_WITH_TESTING=ON"
 fi
+if [ ! -z "$BUILD_WITH_COVERAGE" ]; then
+    cmake_options="$cmake_options -DBUILD_WITH_COVERAGE=ON"
+fi
 
 cmake "$srcdir" -GNinja -DCMAKE_BUILD_TYPE=Debug $cmake_options
 ninja
