a791b9de6a2a365e216183a562ad3369c4ee662b
Max Mertens <max.mail@dameweb.de>
2024-11-12 21:51:45 +0100

    Deconstruct and check for memleaks in shared data

--- src/stst_shared.cpp
+++ src/stst_shared.cpp
@@ -1,4 +1,5 @@
 #include "structstore/stst_shared.hpp"
+#include "structstore/stst_alloc.hpp"
 
 using namespace structstore;
 
@@ -297,6 +298,8 @@ void StructStoreShared::close() {
         bool expected = false;
         // if cleanup == ALWAYS this ensure that unlink is done exactly once
         if (sh_data_ptr->invalidated.compare_exchange_strong(expected, true, std::memory_order_acquire)) {
+            sh_data_ptr->data.~StructStore();
+            sh_data_ptr->mm_alloc.~MiniMalloc();
             if (use_file) {
                 unlink(path.c_str());
             } else {
