ba4e14fcf19523525ea91b17e5ef04d2e2a0cdec
Max Mertens <git@mertemba.de>
2025-09-18 23:17:03 +0200

    Relicensed StructStore under LGPL-3.0-only

--- README.md
+++ README.md
@@ -139,5 +139,14 @@ mmap'ed by several processes.
 
 ## License
 
-This repo is released under the BSD 3-Clause License. See LICENSE file for
-details.
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License v3.0
+as published by the Free Software Foundation.  See LICENSE file.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Lesser Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- src/include/structstore/stst_py.hpp
+++ src/include/structstore/stst_py.hpp
@@ -1,3 +1,18 @@
+// This file is part of the StructStore library.
+// Copyright (C) 2022-2025 Max Mertens
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License v3.0
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Lesser Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef STST_PY_HPP
 #define STST_PY_HPP
 
